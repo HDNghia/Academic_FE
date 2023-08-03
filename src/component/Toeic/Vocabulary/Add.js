9976eef4b996f172dfc5435a71600fb3a6b92239
HDNghia <huynhdangnghia68@gmail.com>
2023-08-03 10:52:10 +0700

    update

--- src/component/Toeic/Vocabulary/Add.js
+++ src/component/Toeic/Vocabulary/Add.js
@@ -15,7 +15,7 @@ function Add(props) {
         numberDate: '0',
         date: date,
         status: '0',
-        part: 'Vocabulary',
+        part: 'vocabulary',
         subject: 'english'
     });
     const [imgQuestion, setImgQuestion] = useState(null);
